c62e6750ebf1a108ce016cccc47395e01129bd85
HaoZ2022 <115562388+HaoZ2022@users.noreply.github.com>
2022-12-09 00:53:32 -0500

    initial commit

---
+++ jquery demo/jquery1.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {alert("[hello hello hello]"); })
+
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
+
+$(document).ready(function() {$("#bananas").draggable(); })
+
+$(document).ready(function() {$("#eyes").draggable(); })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#eggs").draggable(); })
+
+$(document).ready(function() {$("#teapot").draggable(); })
+
+
+
+
+
+
